ee1e86764439a35ee8889930eb6edc75ca2a5d13
Saulo Oliveira <committer@sauloefo.com>
2019-01-07 22:45:28 +0000

    Index page and initial styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,33 @@
 import React from "react"
+import github_logo from "../components/images/github-logo.png"
+import linkedin_logo from "../components/images/linkedin-logo.png"
+import twitter_logo from "../components/images/twitter-logo.png"
+import indexStyle from "./index.module.css";
 
-export default () => <div>Hello world!</div>
+import "./reset.css";
+
+export default () => {
+	return (
+		<div>
+			<div>
+				<h1>Saulo Oliveira</h1>
+				<h2>Solution Architect & Salesforce Specialist</h2>
+			</div>
+			<div>
+				<a href="https://github.com/sauloefo">
+					<img className={indexStyle.socialMediaLogo} src={github_logo} alt="Saulo's GitHub profile"/>
+				</a>
+			</div>
+			<div>
+				<a href="https://linkedin.com/in/sauloefo">
+					<img className={indexStyle.socialMediaLogo} src={linkedin_logo} alt="Saulo's LinkedIn profile"/>
+				</a>
+			</div>
+			<div>
+				<a href="https://twitter.com/sauloefo">
+					<img className={indexStyle.socialMediaLogo} src={twitter_logo} alt="Saulo's twitter profile"/>
+				</a>
+			</div>
+		</div>
+	)
+}
