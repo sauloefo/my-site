51d6630f85adbaa1f901af07df267026319805d2
Saulo Oliveira <committer@sauloefo.com>
2019-03-17 20:01:37 +0000

    Format site with bootstrap

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,49 @@
 import React from "react"
+import email_logo from "../components/images/email-logo.png"
 import github_logo from "../components/images/github-logo.png"
 import linkedin_logo from "../components/images/linkedin-logo.png"
 import twitter_logo from "../components/images/twitter-logo.png"
-import indexStyle from "./index.module.css";
+import indexStyle from "./index.module.css"
 
-import "./reset.css";
+import "bootstrap/dist/css/bootstrap.min.css"
 
 export default () => {
 	return (
-		<div>
-			<div>
+	<div className="container mt-3">
+		<div className="row">
+			<div className="col">
 				<h1>Saulo Oliveira</h1>
-				<h2>Solution Architect & Salesforce Specialist</h2>
+				<h4>Solution Architect & Salesforce Specialist</h4>
 			</div>
-			<div>
-				<a href="https://github.com/sauloefo">
-					<img className={indexStyle.socialMediaLogo} src={github_logo} alt="Saulo's GitHub profile"/>
-				</a>
-			</div>
-			<div>
-				<a href="https://linkedin.com/in/sauloefo">
-					<img className={indexStyle.socialMediaLogo} src={linkedin_logo} alt="Saulo's LinkedIn profile"/>
-				</a>
-			</div>
-			<div>
-				<a href="https://twitter.com/sauloefo">
-					<img className={indexStyle.socialMediaLogo} src={twitter_logo} alt="Saulo's twitter profile"/>
-				</a>
+			<div className="col col-auto">
+				<ul className="list-group">
+					<li className="list-group-item">
+						<a href="mailto:contact@sauloefo.com" title="Saulo's email address">
+							<img className={indexStyle.socialMediaLogo} src={email_logo} alt="contact@sauloefo.com"/>
+							<span className="ml-2">contact@sauloefo.com</span>
+						</a>
+					</li>
+					<li className="list-group-item">
+						<a href="https://github.com/sauloefo" title="Saulo's GitHub profile">
+							<img className={indexStyle.socialMediaLogo} src={github_logo} alt="https://github.com/sauloefo"/>
+							<span className="ml-2">github.com/sauloefo</span>
+						</a>
+					</li>
+					<li className="list-group-item">
+						<a href="https://linkedin.com/in/sauloefo" title="Saulo's LinkedIn profile">
+							<img className={indexStyle.socialMediaLogo} src={linkedin_logo} alt="https://linkedin.com/in/sauloefo"/>
+							<span className="ml-2">linkedin.com/in/sauloefo</span>
+						</a>
+					</li>
+					<li className="list-group-item">
+						<a href="https://twitter.com/sauloefo" title="Saulo's twitter profile">
+							<img className={indexStyle.socialMediaLogo} src={twitter_logo} alt="https://twitter.com/sauloefo"/>
+							<span className="ml-2">twitter.com/sauloefo</span>
+						</a>
+					</li>
+				</ul>
 			</div>
 		</div>
+	</div>
 	)
 }
