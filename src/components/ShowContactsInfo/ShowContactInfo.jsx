bdae8b3bff652e6ead55d1f02badc7fcb14e358b
Saulo Oliveira <committer@sauloefo.com>
2019-06-05 17:09:55 +0100

    Add GoodReads logo

--- src/components/ShowContactsInfo/ShowContactInfo.jsx
+++ src/components/ShowContactsInfo/ShowContactInfo.jsx
@@ -1,29 +1,42 @@
-import React from "react"
-import email_logo from "../images/email-logo.png"
-import github_logo from "../images/github-logo.png"
-import linkedin_logo from "../images/linkedin-logo.png"
-import twitter_logo from "../images/twitter-logo.png"
+import React from "react";
+import email_logo from "../images/email-logo.png";
+import github_logo from "../images/github-logo.png";
+import linkedin_logo from "../images/linkedin-logo.png";
+import twitter_logo from "../images/twitter-logo.png";
+import goodreads_logo from "../images/goodreads-logo.png";
 
 const contactInfoLogo = {
-	"email": email_logo,
-	"github": github_logo,
-	"linkedin": linkedin_logo,
-	"twitter": twitter_logo 
-}
+  email: email_logo,
+  github: github_logo,
+  linkedin: linkedin_logo,
+  twitter: twitter_logo,
+  goodreads: goodreads_logo
+};
 
 export default ({ contactInfo }) => {
-	const { type, emailAddress, href, description, openInNewTab, imageCssClass, label } = contactInfo;
-	const aProps = (type === "email")
-		? {
-			href: `mailto:${emailAddress}`,
-		}
-		: {
-			href,
-			target: openInNewTab && "_blank",
-			rel: openInNewTab && "noopener"
-		}
-	return <a title={description} {...aProps}>
-		<img className={imageCssClass} src={contactInfoLogo[type]} alt={href}/>
-		<span className="ml-2">{label}</span>
-	</a>
-}
+  const {
+    type,
+    emailAddress,
+    href,
+    description,
+    openInNewTab,
+    imageCssClass,
+    label
+  } = contactInfo;
+  const aProps =
+    type === "email"
+      ? {
+          href: `mailto:${emailAddress}`
+        }
+      : {
+          href,
+          target: openInNewTab && "_blank",
+          rel: openInNewTab && "noopener"
+        };
+  return (
+    <a title={description} {...aProps}>
+      <img className={imageCssClass} src={contactInfoLogo[type]} alt={href} />
+      <span className="ml-2">{label}</span>
+    </a>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,57 +1,65 @@
-import React from "react"
-import indexStyle from "./index.module.css"
+import React from "react";
+import indexStyle from "./index.module.css";
 
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import ShowContactsInfo from "../components/ShowContactsInfo/"
+import ShowContactsInfo from "../components/ShowContactsInfo/";
 
 const contactsInfo = [
-	{
-		type: "email",
-		emailAddress: "contact@sauloefo.com",
-		description: "Send an email to Saulo",
-		openInNewTab: false,
-		imageCssClass: indexStyle.socialMediaLogo,
-		label: "contact@sauloefo.com"
-	},
-	{
-		type: "github",
-		href: "https://github.com/sauloefo",
-		description: "Go to Saulo's GitHub profile",
-		openInNewTab: true,
-		imageCssClass: indexStyle.socialMediaLogo,
-		label: "github.com/sauloefo"
-	},
-	{
-		type: "linkedin",
-		href: "https://linkedin.com/in/sauloefo",
-		description: "Go to Saulo's LinkedIn profile",
-		openInNewTab: true,
-		imageCssClass: indexStyle.socialMediaLogo,
-		label: "linkedin.com/in/sauloefo"
-	},
-	{
-		type: "twitter",
-		href: "https://twitter.com/sauloefo",
-		description: "Go to Saulo's Twitter page",
-		openInNewTab: true,
-		imageCssClass: indexStyle.socialMediaLogo,
-		label: "twitter.com/sauloefo"
-	}
-]
+  {
+    type: "github",
+    href: "https://github.com/sauloefo",
+    description: "Go to Saulo's GitHub profile",
+    openInNewTab: true,
+    imageCssClass: indexStyle.socialMediaLogo,
+    label: "github.com/sauloefo"
+  },
+  {
+    type: "linkedin",
+    href: "https://linkedin.com/in/sauloefo",
+    description: "Go to Saulo's LinkedIn profile",
+    openInNewTab: true,
+    imageCssClass: indexStyle.socialMediaLogo,
+    label: "linkedin.com/in/sauloefo"
+  },
+  {
+    type: "twitter",
+    href: "https://twitter.com/sauloefo",
+    description: "Go to Saulo's Twitter page",
+    openInNewTab: true,
+    imageCssClass: indexStyle.socialMediaLogo,
+    label: "twitter.com/sauloefo"
+  },
+  {
+    type: "goodreads",
+    href: "https://goodreads.com/sauloefo",
+    description: "Go to Saulo's Good Reads profile",
+    openInNewTab: true,
+    imageCssClass: indexStyle.socialMediaLogo,
+    label: "goodreads.com/sauloefo"
+  },
+  {
+    type: "email",
+    emailAddress: "contact@sauloefo.com",
+    description: "Send an email to Saulo",
+    openInNewTab: false,
+    imageCssClass: indexStyle.socialMediaLogo,
+    label: "contact@sauloefo.com"
+  }
+];
 
 export default () => {
-	return (
-	<div className="container mt-3">
-		<div className="row">
-			<div className="col col-12 col-lg">
-				<h1>Saulo Oliveira</h1>
-				<h4>Solution Architect & Salesforce Specialist</h4>
-			</div>
-			<div className="col col-lg-auto">
-				<ShowContactsInfo contactsInfo={contactsInfo}></ShowContactsInfo>
-			</div>
-		</div>
-	</div>
-	)
-}
+  return (
+    <div className="container mt-3">
+      <div className="row">
+        <div className="col col-12 col-lg">
+          <h1>Saulo Oliveira</h1>
+          <h4>Solution Architect & Salesforce Specialist</h4>
+        </div>
+        <div className="col col-lg-auto">
+          <ShowContactsInfo contactsInfo={contactsInfo} />
+        </div>
+      </div>
+    </div>
+  );
+};
